4808b025e61fc6c018b64114695a90b1b8d525df
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-01-03 12:04:21 +0530

    Initial Commit

---
+++ src/test/java/com/StarkIndustries/SpringOrmMark1/SpringOrmMark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarkIndustries.SpringOrmMark1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrmMark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
